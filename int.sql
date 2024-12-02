32043456407255f7c43cbd0eed7ba7c004332422
Nathan-Brito <nathansilvabrito@hotmail.com>
2024-12-02 18:19:47 -0400

    new file:   int.sql

---
+++ int.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS logs (
+    id SERIAL PRIMARY KEY,
+    ip VARCHAR(50) NOT NULL,
+    image BYTEA NOT NULL,
+    extracted_text TEXT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
